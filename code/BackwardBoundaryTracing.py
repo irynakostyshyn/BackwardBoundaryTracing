093385f1a4703045bd70b4d2900e90395d4a2baa
irynakostyshyn <i.kostyshyn@ucu.edu.ua>
2017-12-14 19:27:47 +0200

    finally

--- code/BackwardBoundaryTracing.py
+++ code/BackwardBoundaryTracing.py
@@ -1,73 +1,103 @@
 from PIL import Image
-
+import math
 
 class BackwardBoundaryTracing:
-    def __init__(self):
-        self.arr_fon = []
-        self.arr_countour = []
+    def __init__(self, photo):
+        link = "../data/"
+        self.img = Image.open(link + photo)
+        self.image = self.img.load()
 
-    def draw_countour(self, width, height):
-        img = Image.new('RGB', (width, height))
+        self.cont = []
+        self.fone = []
+        self.height = self.img.size[1]
+        self.width = self.img.size[0]
+        self.path = [(-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0)]
+        self.end = None
 
-        for pixel in self.arr_countour:
-            img.putpixel(pixel[0], 900)
+    def start(self):
+        self.tracing()
+        self.draw_countour()
+
+    def draw_countour(self):
+        img = Image.new('RGB', (self.width, self.height))
+
+        for pixel in self.cont:
+            img.putpixel(pixel, (255, 255, 255))
         img.show()
 
-    def create_map(self,photo):
-        link = "../data/"
-        image = Image.open(link + photo)
-
-        pixels = image.load()
-
-        width = image.size[0]
-        height = image.size[1]
-        return pixels, width, height
-
-    def backward(self,width, height, pixels):
-        p_s = p_active = self.search_start_p(width, height, pixels)
-        p_n1 = self.search_clockwise(p_s)
-        p_n2 = self.search_conter_clockwise(p_s)
-        if p_n1 == p_n2:
-            self.arr_fon.append(p_active)
-            self.backward(width, height, pixels)
-        elif p_n1 != p_n2:
-            p_end = p_s
-            p_active = p_n1
-            self.arr_countour.append(p_n1)
+    def tracing(self):
+        active = self.find_start()
+        neighbour_c = self.search_clockwise(active)
+        neighbour_cc = self.search_cclockwise(active)
+        index = 0
         while True:
-            p_contour_neighbour = self.search_coutour_neighbour(p_active)
-            if p_contour_neighbour != p_end:
-                break
-            if p_contour_neighbour in self.arr_countour and p_contour_neighbour != p_end:
-                self.arr_countour.remove(p_contour_neighbour)
-                self.arr_fon.append(p_contour_neighbour)
-                p_active = self.arr_countour[-1]
+            if neighbour_c != neighbour_cc:
+                start = active
+                self.cont.append(start)
+                self.end = start
+                self.cont.append(neighbour_c)
+                active = neighbour_c
+                while active != self.end:
+                    pixik, x = self.search_clockwise(active, 1, index)
+                    active = pixik
+                    index = x
+                    if active not in self.cont and active not in self.fone:
+                        self.cont.append(active)
             else:
-                self.arr_countour.append(p_contour_neighbour)
+                while neighbour_c != neighbour_cc:
+                    self.fone.append(active)
+                    active = neighbour_c
+                    neighbour_c = self.search_clockwise(active)
+                    neighbour_cc = self.search_cclockwise(active)
+            break
 
-        if p_contour_neighbour == p_end and len(self.arr_countour) > 1:
-            return self.arr_countour
+
+    def find_start(self):
+        for i in range(self.width):
+            for k in range(self.height):
+                if not BackwardBoundaryTracing.is_grey(self.image[i, k]) and self.image[i, k] not in self.fone:
+                    return (i, k)
+
+    def search_clockwise(self, pixel, check=None, k=0):
+        first, second = pixel
+        if check:
+            k = (k + 6) % 8
+            for i in range(k, len(self.path) + k):
+                try:
+                    if i > len(self.path) - 1:
+                        i -= len(self.path)
+                    if not BackwardBoundaryTracing.is_grey(self.image[first + self.path[i][0], second + self.path[i][1]]):
+                        if (len(self.cont) > 5 and (first + self.path[i][0], second + self.path[i][1]) == self.end) or (
+                                (first + self.path[i][0], second + self.path[i][1]) not in self.cont and (
+                            first + self.path[i][0], second + self.path[i][1]) not in self.fone):
+                            self.cont.append((first, second))
+                            return (first + self.path[i][0], second + self.path[i][1]), i
+                except IndexError:
+                    continue
+            pix = self.cont.pop()
+            self.fone.append(pix)
+            return self.cont[-1], 0
         else:
-            self.backward(width, height, pixels)
+            for i in self.path:
+                try:
+                    if not BackwardBoundaryTracing.is_grey(self.image[first + i[0], second + i[1]]) and (first + i[0], second + i[1]) not in self.cont:
+                        return (first + i[0], second + i[1])
+                except IndexError:
+                    continue
 
-    def search_start_p(self, width, height, pixels):
-        for i in range(width):
-            for j in range(height):
-                if not self.black_pixel(pixels[i, j]) and [i, j] not in self.arr_fon:
-                    return [i, j]
-
-    def search_clockwise(self,pixel):
-        pass  # OLES
-
-    def search_conter_clockwise(self,pixel):
-        pass  # OLES
-
-    def search_coutour_neighbour(self,pixel):
-        pass  # OLES
+    def search_cclockwise(self, pixel):
+        first, second = pixel
+        for i in range(0, -8, -1):
+            try:
+                if not BackwardBoundaryTracing.is_grey(self.image[first + self.path[i][0], second + self.path[i][1]]):
+                    if (first + self.path[i][0], second + self.path[i][1]) not in self.cont:
+                        return (first + self.path[i][0], second + self.path[i][1])
+            except IndexError:
+                continue
 
     @staticmethod
-    def black_pixel(pixel):
-        black = [(0 + i, 0 + i, 0 + i) for i in range(13)]
-        return pixel in black
+    def is_grey(rgb):
+        return math.sqrt(rgb[0] ** 2 + rgb[1] ** 2 + rgb[2] ** 2) < 21
 
-    # зберігати ключ пікселя, доробити пошук
+bb = BackwardBoundaryTracing("4.jpg")
+bb.start()
